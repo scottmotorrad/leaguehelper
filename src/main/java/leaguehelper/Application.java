8b84b0b8e409622febcb987e4dad7beb3e3f0463
scottmotorrad <scottruhnau88@gmail.com>
2017-02-27 18:07:20 -0800

    Initial Springfox swagger setup

--- src/main/java/leaguehelper/Application.java
+++ src/main/java/leaguehelper/Application.java
@@ -1,11 +1,50 @@
 package leaguehelper;
 
+import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {LeagueHelperController.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(leagueHelperPaths())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("League Helper API")
+                .description("Coming Soon")
+                .contact(new Contact("Scott", "url", "email"))
+                .license("None")
+                .licenseUrl("None")
+                .version("0.1")
+                .build();
+    }
+
+    private Predicate<String> leagueHelperPaths() {
+        return or(regex("/api/v1/.*"));
     }
 }
