7932983433f0a17c97a108c1bb77efb72fb6e578
Scott <scottruhnau88@gmail.com>
2017-02-28 00:22:04 -0800

    Apply a lightly modified version of Google's Java style guide (#10)

--- src/main/java/leaguehelper/LeagueHelperController.java
+++ src/main/java/leaguehelper/LeagueHelperController.java
@@ -6,20 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class LeagueHelperController {
 
     private final AtomicLong counter = new AtomicLong();
-    private final static String prefix = "/api/v1";
+    private static final String prefix = "/api/v1";
 
     @RequestMapping(path = (prefix + "/sample"), method = RequestMethod.GET, produces = "application/json")
     @ApiOperation(value = "exactly")
     public SampleDTO getSample(
             @ApiParam(value = "precisely")
-            @RequestParam(value="name", defaultValue="World") String name) {
+            @RequestParam(value = "name", defaultValue = "World") String name) {
         return new SampleDTO(name.concat((((Long) counter.getAndAdd(1)).toString())));
     }
 }
