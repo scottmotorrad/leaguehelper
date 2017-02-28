d000094dec3d2b890fe467169dc926f0f38f50ca
scottmotorrad <scottruhnau88@gmail.com>
2017-02-28 00:19:31 -0800

    Apply a lightly modified version of Google's Java style guide

--- src/main/java/leaguehelper/Application.java
+++ src/main/java/leaguehelper/Application.java
@@ -1,5 +1,8 @@
 package leaguehelper;
 
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +14,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableSwagger2
--- src/main/java/leaguehelper/HomeController.java
+++ src/main/java/leaguehelper/HomeController.java
@@ -1,4 +1,5 @@
 package leaguehelper;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
